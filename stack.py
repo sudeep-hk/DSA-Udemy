4292e1f0f45365ace38ec6df6dacf97bb0e0c730
Sudeep Kumar <sudeep.kumar@bold.com>
2022-07-02 20:17:56 +0530

    stk

---
+++ stack.py
@@ -0,0 +1,7 @@
+#lifo 
+#add content in the last of the array then it is stack
+#delete only last element
+#array support all type of deletion
+#pop- remove last item
+#peek==top, last item availaible in the stack
+#push- insert at last
