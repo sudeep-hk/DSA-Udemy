c0f8aec39972026e5549b001e408006433d37393
Sudeep Kumar <sudeep.kumar@bold.com>
2022-07-02 18:55:17 +0530

    doubly ll

---
+++ singll.py
@@ -0,0 +1,29 @@
+from requests import delete
+
+
+class Node:
+    def __init__(self,value=None,next=None):
+        self.value=value
+        self.next=next
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def insert():
+        pass
+    def delete():
+        pass
+    
+
+
+n1=Node(3)
+n2=Node(7)
+n3=Node(2)
+
+LL=LinkedList()
+LL.head=n1
+n1.next=n2
+n2.next=n3
+
+
+
